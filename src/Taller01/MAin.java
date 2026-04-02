53fafb23c02864c5b834b40ac043b83ea1438923
poktetto <pottketo@gmail.com>
2026-04-02 19:43:31 -0300

    cambio

--- src/Taller01/MAin.java
+++ src/Taller01/MAin.java
@@ -5,10 +5,40 @@ import java.io.FileNotFoundException;
 
 public class MAin {
 	public static void main(String[] args) {
-		System.out.println("1) Menu de Usuarios");
-		System.out.println("2) Menu de Analisis");
-		System.out.println("3) Salir");
-		System.out.print(">");//hi
+	// crear para archivos
+		File archUsuario= new File ("d:/txts/Usuarios.txt");
+		
+		File archRegistro=new File ("d:/txts/Registros.txt");
+		Scanner s = new Scanner (System.in);
+		int menuPrincipal;
+		
+		do {  //para crear menu principal
+			System.out.println("1) Menu de Usuarios");
+			System.out.println("2) Menu de Analisis");
+			System.out.println("3) Salir");
+			
+			menuPrincipal= s.nextInt();
+			
+			while (menuPrincipal!=3) { //crear los 2 menus
+				
+				
+				
+				
+			}
+			
+			
+			
+			
+		}while (menuPrincipal!=1 && menuPrincipal!=2 &&menuPrincipal!=3 );
+		
+		 
+		
+		
+		
+		
+		
+		
 
 	}
+
 }
