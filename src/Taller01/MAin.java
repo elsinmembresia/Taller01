2d7450ce320a4428d5a6bc6f6669daa2063bad3c
elsinmembresia <elsinmembresia@gmail.com>
2026-04-02 19:22:17 -0300

    test

--- src/Taller01/MAin.java
+++ src/Taller01/MAin.java
@@ -8,6 +8,6 @@ public class MAin {
 		System.out.println("1) Menu de Usuarios");
 		System.out.println("2) Menu de Analisis");
 		System.out.println("3) Salir");
-		
+		System.out.print(">");
 	}
 }
