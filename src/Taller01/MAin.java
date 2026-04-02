e6d801be23557f9b010b670bbb7df8fa00e4170d
elsinmembresia <elsinmembresia@gmail.com>
2026-04-02 18:41:54 -0300

    se añadieron archivos iniciales

---
+++ src/Taller01/MAin.java
@@ -0,0 +1,13 @@
+package Taller01;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MAin {
+	public static void main(String[] args) {
+		System.out.println("1) Menu de Usuarios");
+		System.out.println("2) Menu de Analisis");
+		System.out.println("3) Salir");
+		
+	}
+}
